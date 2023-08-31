37dcdb688947673a8ea123cd2bd8dcba5e5231bb
Geazi <oigeazi@gmail.com>
2023-08-31 19:39:40 -0300

    Feat: Configuração de rotas

--- base/src/index.tsx
+++ base/src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Main from './pages/Main';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import MainRoutes from "./routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Main />
+    <BrowserRouter>
+      <MainRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 );
